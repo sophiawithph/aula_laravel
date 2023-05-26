8fda2c4d8263a23d346be28ad1ec36668c7c6935
Rafael Jaques <rafael@phpit.com.br>
2023-05-26 11:03:22 -0300

    Views de Produtos

---
+++ resources/views/produtos/add.blade.php
@@ -0,0 +1,19 @@
+{{-- views/produtos/add.blade.php --}}
+@extends('includes.base')
+
+@section('title', 'Produtos - Adicionar')
+
+@section('content')
+    <h2>Adicione seu produto</h2>
+
+    <form action="{{ route('produtos.addSave') }}" method="post">
+        @csrf
+        <input type="text" name="name" placeholder="Nome do produto">
+        <br>
+        <input type="number" name="price" step="0.01" placeholder="Preço" min="0">
+        <br>
+        <input type="number" name="quantity" placeholder="Quantidade" min="0">
+        <hr width="30%" align="left">
+        <input type="submit" value="Gravar">
+    </form>
+@endsection
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -1,2 +1,9 @@
+{{-- resources/views/produtos/index.blade.php --}}
 @extends('includes.base')
 
+@section('title', 'Produtos')
+
+@section('content')
+<p>Produtos funcionando</p>
+<a href="{{ route('produtos.add') }}">Adicionar produto</a>
+@endsection
